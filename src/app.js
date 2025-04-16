d32f880eb159d0948765f917fd24201b8b7aa59e
SW-64 <ettjddns6@gmail.com>
2025-04-16 23:32:36 +0900

    TEST : test routing response 값 출력 변경

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(globalErrorHandler); // 미들웨어 중, 에러처리 미들웨어는 �
 
 // Test Routing
 app.get('/', (req, res) => {
-  return res.json('hello world CICD test 2 ');
+  return res.json('hello world CICD test 3 ');
 });
 
 app.listen(port, () => {
