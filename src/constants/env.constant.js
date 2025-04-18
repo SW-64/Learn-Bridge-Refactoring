f5284c52dcd00ac1bbc928bb52d48115af1cfb62
SW-64 <ettjddns6@gmail.com>
2025-04-18 18:24:53 +0900

    UPDATE : env example 파일 설정 및 값 추가

--- src/constants/env.constant.js
+++ src/constants/env.constant.js
@@ -3,3 +3,6 @@ import 'dotenv/config';
 export const SERVER_PORT = process.env.SERVER_PORT;
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+export const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID;
+export const KAKAO_CLIENT_SECRET = process.env.KAKAO_CLIENT_SECRET;
+export const KAKAO_CALLBACK_URL = process.env.KAKAO_CALLBACK_URL;
